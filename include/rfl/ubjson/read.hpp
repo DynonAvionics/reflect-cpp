e743224b67327354ce60449c3f595f87380cc6c7
Bryce Schober <bryce.schober@dynon.com>
2025-08-06 15:31:12 -0700

    Use non-throwing `try_decode_ubjson()` if available

--- include/rfl/ubjson/read.hpp
+++ include/rfl/ubjson/read.hpp
@@ -5,6 +5,7 @@
 #include <istream>
 #include <jsoncons/json.hpp>
 #include <jsoncons_ext/ubjson/decode_ubjson.hpp>
+#include <rfl/thirdparty/jsoncons_config.hpp>
 #include <string>
 
 #include "../Processors.hpp"
@@ -17,35 +18,77 @@ namespace rfl::ubjson {
 using InputObjectType = typename Reader::InputObjectType;
 using InputVarType = typename Reader::InputVarType;
 
-/// Parses an object from UBJSON using reflection.
+#if RFL_JSONCONS_PROVIDES_TRY_DECODE
+// Newer versions of jsoncons provide std::expected-like for error handling instead of exceptions
+
+/// Parses an object from UBJSON data blob, handling jsoncons std::expected-like in newer versions
 template <class T, class... Ps>
 Result<internal::wrap_in_rfl_array_t<T>> read(const std::vector<char>& _bytes) {
-  // TODO: Use a non-throwing decode_ubjson(), pending https://github.com/danielaparker/jsoncons/issues/615
+  auto result = jsoncons::ubjson::try_decode_ubjson<jsoncons::json>(_bytes);
+  if (result.has_value()) {
+    auto r = Reader();
+    return Parser<T, Processors<Ps...>>::read(r, InputVarType{&result.value()});
+  } else {
+    // std::string error("Could not parse UBJSON: ");
+    // FIXME: Remove temporary test output discriminator
+    std::string error("Could not parse UBJSON (result had error): ");
+    error.append(jsoncons::to_string(result.error()));
+    return rfl::error(error);
+  }
+}
+
+/// Parses an object from a stream, handling jsoncons std::expected-like in newer versions
+template <class T, class... Ps>
+Result<internal::wrap_in_rfl_array_t<T>> read(std::istream& _stream) {
+  auto result = jsoncons::ubjson::try_decode_ubjson<jsoncons::json>(_stream);
+  if (result.has_value()) {
+    auto r = Reader();
+    return Parser<T, Processors<Ps...>>::read(r, InputVarType{&result.value()});
+  } else {
+    // std::string error("Could not parse UBJSON: ");
+    // FIXME: Remove temporary test output discriminator
+    std::string error("Could not parse UBJSON (result had error): ");
+    error.append(jsoncons::to_string(result.error()));
+    return rfl::error(error);
+  }
+}
+
+#else // RFL_JSONCONS_PROVIDES_TRY_DECODE
+// Older versions of jsoncons throw exceptions on errors, so we need to catch them
+
+/// Parses an object from UBJSON data blob, catching jsoncons exceptions in older versions
+template <class T, class... Ps>
+Result<internal::wrap_in_rfl_array_t<T>> read(const std::vector<char>& _bytes) {
   try {
     auto val = jsoncons::ubjson::decode_ubjson<jsoncons::json>(_bytes);
     auto r = Reader();
     return Parser<T, Processors<Ps...>>::read(r, InputVarType{&val});
   } catch(const jsoncons::ser_error& e)  {
-    std::string error("Could not parse UBJSON: ");
+    // std::string error("Could not parse UBJSON: ");
+    // FIXME: Remove temporary test output discriminator
+    std::string error("Could not parse UBJSON (trapped exception): ");
     error.append(e.what());
     return rfl::error(error);
   }
 }
 
-/// Parses an object from a stream.
+/// Parses an object from a stream, catching jsoncons exceptions in older versions
 template <class T, class... Ps>
 Result<internal::wrap_in_rfl_array_t<T>> read(std::istream& _stream) {
-  // TODO: Use a non-throwing decode_ubjson(), pending https://github.com/danielaparker/jsoncons/issues/615
   try {
     auto val = jsoncons::ubjson::decode_ubjson<jsoncons::json>(_stream);
     auto r = Reader();
     return Parser<T, Processors<Ps...>>::read(r, InputVarType{&val});
   } catch(const jsoncons::ser_error& e)  {
-    std::string error("Could not parse UBJSON: ");
+    // std::string error("Could not parse UBJSON: ");
+    // FIXME: Remove temporary test output discriminator
+    std::string error("Could not parse UBJSON (trapped exception): ");
     error.append(e.what());
     return rfl::error(error);
   }
 }
+
+#endif // RFL_JSONCONS_PROVIDES_TRY_DECODE
 
 }  // namespace rfl::ubjson
 
--- tests/ubjson/test_error_messages.cpp
+++ tests/ubjson/test_error_messages.cpp
@@ -16,12 +16,39 @@ struct Person {
   std::vector<Person> children;
 };
 
-TEST(ubjson, test_field_error_messages) {
+TEST(ubjson, test_empty_field_error_messages) {
+  // Use JSON input to generic parser for convenient flexible test input
+  const std::string faulty_string =
+      R"({})";
+  const auto faulty_generic = rfl::json::read<rfl::Generic>(faulty_string);
+  const auto faulty_ubjson = rfl::ubjson::write(faulty_generic);
+
+  rfl::Result<Person> result = rfl::error("result didn't get set");
+  EXPECT_NO_THROW({
+    result = rfl::ubjson::read<Person>(faulty_ubjson);
+  });
+
+    const std::string expected = R"(Found 4 errors:
+1) Field named 'firstName' not found.
+2) Field named 'lastName' not found.
+3) Field named 'birthday' not found.
+4) Field named 'children' not found.)";
+  EXPECT_EQ(result.error().what(), expected);
+
+  EXPECT_TRUE(!result.has_value() && true);
+}
+
+TEST(ubjson, test_field_type_error_messages) {
+  // Use JSON input to generic parser for convenient flexible test input
   const std::string faulty_string =
       R"({"firstName":"Homer","lastName":12345,"birthday":"04/19/1987"})";
   const auto faulty_generic = rfl::json::read<rfl::Generic>(faulty_string);
   const auto faulty_ubjson = rfl::ubjson::write(faulty_generic);
-  const auto result = rfl::ubjson::read<Person>(faulty_ubjson);
+
+  rfl::Result<Person> result = rfl::error("result didn't get set");
+  EXPECT_NO_THROW({
+    result = rfl::ubjson::read<Person>(faulty_ubjson);
+  });
 
   // Order of errors is different than input JSON because rfl::Generic doesn't preserve order
   const std::string expected = R"(Found 3 errors:
@@ -35,10 +62,8 @@ TEST(ubjson, test_field_error_messages) {
 }
 
 TEST(ubjson, test_decode_error_without_exception) {
-  const std::string good_string =
-      R"({"firstName":"Homer","lastName":"Simpson","birthday":"1987-04-19"})";
-  const auto good_generic = rfl::json::read<rfl::Generic>(good_string);
-  auto faulty_ubjson = rfl::ubjson::write(good_generic);
+  const Person homer{"Homer", "Simpson", "1987-04-19"};
+  auto faulty_ubjson = rfl::ubjson::write(homer);
   faulty_ubjson[0] = '\xff';  // Corrupt structure of ubjson encoding
 
   rfl::Result<Person> result = rfl::error("result didn't get set");
@@ -46,8 +71,9 @@ TEST(ubjson, test_decode_error_without_exception) {
     result = rfl::ubjson::read<Person>(faulty_ubjson);
   });
 
-  // A proposal: A generic prefix, followed by the underlying library's error output
-  const std::string expected = R"(Could not parse UBJSON: Unknown type at position 1)";
+  // const std::string expected = R"(Could not parse UBJSON: Unknown type at position 1)";
+  // FIXME: Remove temporary test output discriminator
+  const std::string expected = R"(Could not parse UBJSON (trapped exception): Unknown type at position 1)";
   EXPECT_EQ(result.error().what(), expected);
 
   EXPECT_TRUE(!result.has_value() && true);
